388a6081010f7d3473eea0f3496a2ecdb7b5345d
Fabiola Spredemann <fabispredemann@gmail.com>
2023-02-20 17:28:29 -0300

    Day 0: Hello, World.

---
+++ day0.py
@@ -0,0 +1,22 @@
+# Day 0: Hello, World.
+
+# Task
+# To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, 
+# and finally print the value of your variable on a second line.
+# Note: The instructions are Java-based, but we support submissions in many popular languages.
+#  You can switch languages using the drop-down menu above your editor, and the inputString variable may be written 
+# differently depending on the best-practice conventions of your submission language.
+
+# Input Format
+
+# A single line of text denoting inputString (the variable whose contents must be printed).
+
+# Output Format
+
+# Print Hello, World. on the first line, and the contents of inputString on the second line.
+
+input_string = input()
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
