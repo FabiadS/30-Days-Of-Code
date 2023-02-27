e7fc8c49565b462be27388a94d5c36e911072e80
Fabiola Spredemann <fabispredemann@gmail.com>
2023-02-27 12:58:22 -0300

    Day 7: Arrays

---
+++ day7.py
@@ -0,0 +1,34 @@
+# Objective
+# Today, we will learn about the Array data structure.
+
+# Task
+# Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+# Example
+# A = [1,2,3,4]
+
+# Print 4 3 2 1. Each integer is separated by one space.
+
+# Input Format
+
+# The first line contains an integer, N (the size of our array).
+# The second line contains N space-separated integers that describe array A's elements.
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    
+    print(*arr[::-1])
+        
